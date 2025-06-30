3750e3a4900491b0c531c806b640692b9e197796
melrwa <orwamelkizedek@gmail.com>
2025-06-30 04:55:17 +0300

    Complete React Native Stickersmash

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,31 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack, Link } from 'expo-router'
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
+const NotFoundScreen = () => {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <Stack.Screen options={{title:'Oops! Not Found'}} />
+      <View style={styles.container}>
+         <Link href='/' style={styles.button}>Go back To Home Screen</Link>
+      </View>
+     
     </>
-  );
+  )
 }
 
+export default NotFoundScreen
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+    container:{
+        flex:1,
+        backgroundColor:'#25292e',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    button:{
+        fontSize:20,
+        textDecorationLine:'underline',
+        color:'#fff'
+    }
+})
---
+++ app/components/ImageViewer.tsx
@@ -0,0 +1,19 @@
+import {  ImageSourcePropType, StyleSheet } from 'react-native'
+import React from 'react'
+import { Image } from 'expo-image'
+type Props={
+    imgSource:ImageSourcePropType;
+    selectedImage?:string;
+}
+
+export default function ImageViewer({imgSource, selectedImage }:Props) {
+    const imageSource=selectedImage? {uri: selectedImage}:imgSource
+  return  <Image source={imageSource} style={styles.image} />
+}
+const styles=StyleSheet.create({
+    image:{
+        width:320,
+        height:440,
+        borderRadius:18,
+    }
+})
